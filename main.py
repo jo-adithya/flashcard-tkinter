d6bebf579b89d752b63c79371efeaea266af55f6
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-16 18:06:00 +0700

    create next card functionality

--- main.py
+++ main.py
@@ -5,14 +5,29 @@ FONT_TEXT = ('Arial', 60, 'bold')
 FONT_TITLE = ('Arial', 30, 'italic')
 BG = '#f4f4f4'
 df = pd.read_csv('data/french_words.csv')
+current_card = df.sample()
 
 # ------------------------- CUSTOM FUNCTIONS ------------------------- #
+def next_card():
+    global flip_timer, current_card
+    window.after_cancel(flip_timer)
+    current_card = df.sample()
+    canvas.itemconfig(card, image=front_card_img)
+    canvas.itemconfig(card_title, text='French', fill='black')
+    canvas.itemconfig(card_text, text=current_card.iloc[0]['French'], fill='black')
+    flip_timer = window.after(3000, show_answer)
+
+def show_answer():
+    canvas.itemconfig(card, image=back_card_img)
+    canvas.itemconfig(card_title, text='English', fill='white')
+    canvas.itemconfig(card_text, text=current_card.iloc[0]['English'], fill='white')
 
 # ------------------------- BUTTON FUNCTIONS ------------------------- #
 
 # ----------------------------- UI SETUP ----------------------------- #
 window = Tk()
 window.config(padx=50, pady=50, bg=BG)
+flip_timer = window.after(3000, show_answer)
 
 # Images
 front_card_img = PhotoImage(file='images/front_card.png')
@@ -22,15 +37,17 @@ unlike_img = PhotoImage(file='images/unlike_btn.png')
 
 # Canvas
 canvas = Canvas(width=700, height=500, highlightthickness=0)
-canvas.create_image(350, 250, image=front_card_img)
-title_text = canvas.create_text(350, 150, text='French', font=FONT_TITLE)
-word_text = canvas.create_text(350, 250, text='trouve', font=FONT_TEXT)
+card = canvas.create_image(350, 250, image=front_card_img)
+card_title = canvas.create_text(350, 150, text='', font=FONT_TITLE)
+card_text = canvas.create_text(350, 250, text='', font=FONT_TEXT)
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Buttons
-like_btn = Button(image=like_img, borderwidth=0, highlightthickness=0, bd=0, command=new_vocab)
-unlike_btn = Button(image=unlike_img, borderwidth=0, highlightthickness=0, bd=0, command=new_vocab)
+like_btn = Button(image=like_img, borderwidth=0, highlightthickness=0, bd=0, command=next_card)
+unlike_btn = Button(image=unlike_img, borderwidth=0, highlightthickness=0, bd=0, command=next_card)
 like_btn.grid(row=1, column=1)
 unlike_btn.grid(row=1, column=0)
+
+next_card()
 
 window.mainloop()
